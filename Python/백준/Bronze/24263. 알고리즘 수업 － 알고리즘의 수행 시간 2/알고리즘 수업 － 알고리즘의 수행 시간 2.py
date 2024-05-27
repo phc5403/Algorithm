f5d67d5065ddd3421e87ea62bf5b06e7c5b8fe71
HEECO <33312417+phc5403@users.noreply.github.com>
2024-05-27 15:36:41 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.py
@@ -0,0 +1,14 @@
+def MenOfPassion_execution(n):
+    # 코드1의 수행 횟수는 n입니다.
+    execution_count = n
+    
+    # 다항식의 최고차항의 차수는 1입니다.
+    highest_degree = 1
+    
+    # 결과 출력
+    print(execution_count)
+    print(highest_degree)
+
+# 입력 받기
+n = int(input())
+MenOfPassion_execution(n)
