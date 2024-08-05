dab3444f0d7951ce7bb1fac9877e1684780c6d5e
HEECO <33312417+phc5403@users.noreply.github.com>
2024-08-06 07:20:47 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 56 ms, Memory: 34044 KB -BaekjoonHub

---
+++ Python/백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.py
@@ -0,0 +1,34 @@
+from sys import stdin
+from collections import deque
+
+
+def bfs():
+    qu = deque()
+    qu.append([0, 0])
+    visited[0][0] = True
+
+    while qu:
+        x, y = qu.popleft()
+
+        if lst[x][y] == -1:
+            return "HaruHaru"
+
+        jump = lst[x][y]
+        for k in range(2):
+            nx = x + dx[k] * jump
+            ny = y + dy[k] * jump
+
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+                visited[nx][ny] = True
+                qu.append([nx, ny])
+
+    return "Hing"
+
+
+N = int(stdin.readline())
+lst = [list(map(int, stdin.readline().split())) for _ in range(N)]
+visited = [[False] * N for _ in range(N)]
+dx = [0, 1]
+dy = [1, 0]
+
+print(bfs())
