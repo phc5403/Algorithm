dec72aa752a51a995c67824bf8a8fbf52aa6842c
HEECO <33312417+phc5403@users.noreply.github.com>
2024-08-07 22:36:19 +0900

    [Bronze II] Title: 분산처리, Time: 1524 ms, Memory: 109240 KB -BaekjoonHub

--- PyPy3/백준/Bronze/1009. 분산처리/분산처리.py
+++ PyPy3/백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,17 +1,14 @@
 from sys import stdin
 
-TC = int(stdin.readline())
+TC = int(stdin.readline().strip())
 
 for tc in range(1, TC + 1):
     a, b = map(int, stdin.readline().split())
 
-    res = a
+    res = 1
 
-    for i in range(2, b+1):
+    for i in range(b):
         res = (res * a) % 10
-
-    if b == 1:
-        res = a % 10
 
     if res == 0:
         res = 10
