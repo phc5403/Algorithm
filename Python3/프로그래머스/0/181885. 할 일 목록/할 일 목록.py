f6901056ee2160fd4f8807428adbfea73ab9180e
HEECO <33312417+phc5403@users.noreply.github.com>
2025-03-03 23:17:18 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,8 @@
+def solution(todo_list, finished):
+    answer = []
+    
+    for idx in range(len(finished)):
+        if not finished[idx]:
+            answer.append(todo_list[idx])
+    
+    return answer
